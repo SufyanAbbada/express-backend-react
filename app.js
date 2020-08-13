60749f5c1af92ecddd5e1503f233ec94b8425e20
Sufyan Abbada <62093556+SufyanAbbada@users.noreply.github.com>
2020-08-13 19:53:04 +0500

    Little changes

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "tim-shop/build")));
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/tim-shop/build/index.html"));
-});
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,6 +24,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/products", productsRouter);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "tim-shop/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/tim-shop/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
